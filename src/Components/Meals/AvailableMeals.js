b8b60ccb9bf99754972f2d91741f5c146d79833d
shrutijain20 <jainshrutibrt1234@gmail.com>
2022-07-10 02:02:39 +0530

    Update AvailableMeals.js

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -3,66 +3,94 @@ import Card from "../UI/Card/Card";
 import classes from './AvailableMeals.module.css';
 import MealItem from "./MealItem/MealItem";
 
+const meals = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 18.99,
+  },
+];
+
 
 
 const AvailableMeals = () =>{
 
-  const [meals,setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  //const [meals,setMeals] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
 
- 
 
-const fetchMealsHandler = useCallback(async() =>{
-  setIsLoading(true);
-  setError(null);
-  try {
-    const response = await fetch('https://food-order-app-fcc45-default-rtdb.firebaseio.com/meals.json');
-    if (!response.ok) {
-      throw new Error('Something went wrong!');
-    }
 
-    const data = await response.json();
-    const DUMMY_MEALS = [] ;
-    for(const key in data){
-      DUMMY_MEALS.push({
-        id: key,
-        name: data[key].name,
-        description: data[key].description,
-        price: data[key].price 
-      });
-    }
-    setMeals(DUMMY_MEALS);
-  }
-  catch (error){
+
+// const fetchMealsHandler = useCallback(async() =>{
+//   setIsLoading(true);
+//   setError(null);
+//   try {
+//     const response = await fetch('https://food-order-app-fcc45-default-rtdb.firebaseio.com/meals.json');
+//     if (!response.ok) {
+//       throw new Error('Something went wrong!');
+//     }
+
+//     const data = await response.json();
+//     const DUMMY_MEALS = [] ;
+//     for(const key in data){
+//       DUMMY_MEALS.push({
+//         id: key,
+//         name: data[key].name,
+//         description: data[key].description,
+//         price: data[key].price 
+//       });
+//     }
+//     setMeals(DUMMY_MEALS);
+//   }
+//   catch (error){
     
-    setError(error.message);
-  }
-  setIsLoading(false);
-} ,[] );
+//     setError(error.message);
+//   }
+//   setIsLoading(false);
+// } ,[] );
 
-useEffect(() => {
-  fetchMealsHandler();
-}, [fetchMealsHandler]);
-
-
-if(error){
-  return(
-    <section className={classes.mealsError}>
-       <p>{error}</p>
-    </section>
-  )
-}
+// useEffect(() => {
+//   fetchMealsHandler();
+// }, [fetchMealsHandler]);
 
 
-if(isLoading){
-  return(
-    <section className={classes.mealsLoading}>
-       <p>Loading......</p>
-    </section>
-  )
-}
+// if(error){
+//   return(
+//     <section className={classes.mealsError}>
+//        <p>{error}</p>
+//     </section>
+//   )
+// }
+
+
+// if(isLoading){
+//   return(
+//     <section className={classes.mealsLoading}>
+//        <p>Loading......</p>
+//     </section>
+//   )
+// }
 
 
     const mealsList = meals.map( (meal) => 
